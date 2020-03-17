d74b1661601ca85bdaf4187137adc3188849317d
X1 Carbon <yjl9903@vip.qq.com>
2020-03-17 22:02:31 +0800

    add: void type, function type

--- src/xlang/type.ts
+++ src/xlang/type.ts
@@ -1,5 +1,7 @@
 export type ValueType = 'numberType' | 'floatType' | 'stringType' | 'boolType';
 
+export type VoidType = 'voidType';
+
 export interface Variable {
   id: number;
   name: string;
@@ -7,9 +9,26 @@ export interface Variable {
   value?: any;
   isArg: boolean;
   isConst: boolean;
+  isGlobal: boolean;
 }
 
 export interface Literal {
   type: ValueType;
   value: any;
 }
+
+export interface UserFunction {
+  type: ValueType | VoidType;
+  args: ValueType[];
+  address: number;
+  name: string;
+}
+
+export interface BuiltinFunction {
+  type: ValueType | VoidType;
+  args: ValueType[];
+  fn: (...args: any[]) => any;
+  name: string;
+}
+
+export type GlobalFunction = UserFunction | BuiltinFunction;
