a84f714f5c1863c66a42a93722ed870e6ec9c030
X1 Carbon <yjl9903@vip.qq.com>
2020-04-01 16:32:50 +0800

    add: cli import library

--- src/cli.ts
+++ src/cli.ts
@@ -5,6 +5,7 @@ import { readFileSync, writeFileSync } from 'fs';
 import { cac } from 'cac';
 
 import { XLang } from './xlang';
+import { BuiltinFunction } from './xlang/type';
 
 const cli = cac('xlang');
 
@@ -12,13 +13,26 @@ cli
   .command('<codePath> [...args]', 'Run XLang')
   .option('-o, --out <outputPath>', 'Compile Output')
   .option('-i, --input <intputPath>', 'Run input')
+  .option('-l, --lib <libPath>', 'Import library functions')
   .action(
-    (codePath, args: string[], option: { out?: string; input?: string }) => {
+    async (
+      codePath,
+      args: string[],
+      option: { out?: string; input?: string; lib?: string }
+    ) => {
       const codeText = readFileSync(codePath, 'utf-8');
       const runtime = new XLang();
       runtime.addFn('print', 'voidType', ['stringType'], (text: string) => {
         console.log(text);
       });
+      if (option.lib !== undefined) {
+        const fns: Array<BuiltinFunction> = (
+          await import(path.resolve(process.cwd(), option.lib))
+        ).lib;
+        for (const fn of fns) {
+          runtime.addFn(fn.name, fn.type, fn.args, fn.fn);
+        }
+      }
       const res = runtime.compile(codeText);
       if (res.ok) {
         if (option.out) {
